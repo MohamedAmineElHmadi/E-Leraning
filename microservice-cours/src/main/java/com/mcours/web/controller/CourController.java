b665290550081b17c20fbf435f7807ba250342d1
unknown <mohamedamine.elhmadi@esprit.tn>
2022-10-24 00:01:07 +0100

    docker

--- microservice-cours/src/main/java/com/mcours/web/controller/CourController.java
+++ microservice-cours/src/main/java/com/mcours/web/controller/CourController.java
@@ -2,11 +2,12 @@ package com.mcours.web.controller;
 
 import com.mcours.dao.CourDao;
 import com.mcours.model.Cour;
+import com.mcours.web.exceptions.ImpossibleAjouterCommandeException;
 import com.mcours.web.exceptions.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,25 +20,36 @@ public class CourController {
 
     // Affiche la liste de tous les cours disponibles
     @GetMapping(value = "/Cours")
-    public List<Cour> listeDesCours(){
+    public List<Cour> listeDesCours() {
 
         List<Cour> cours = courDao.findAll();
 
-        if(cours.isEmpty()) throw new ProductNotFoundException("Aucun cour n'est disponible à la vente");
+        if (cours.isEmpty()) throw new ProductNotFoundException("Aucun cour n'est disponible à la vente");
 
         return cours;
 
     }
 
     //Récuperer un cour par son id
-    @GetMapping( value = "/Cours/{id}")
+    @GetMapping(value = "/Cours/{id}")
     public Optional<Cour> recupererUnCour(@PathVariable int id) {
 
         Optional<Cour> cour = courDao.findById(id);
 
-        if(!cour.isPresent())  throw new ProductNotFoundException("Le cour correspondant à l'id " + id + " n'existe pas");
+        if (!cour.isPresent())
+            throw new ProductNotFoundException("Le cour correspondant à l'id " + id + " n'existe pas");
 
         return cour;
+    }
+
+    @PostMapping(value = "/ajoutcours")
+    public ResponseEntity<Cour> ajouterCour(@RequestBody Cour cour) {
+
+        Cour nouveauCour = courDao.save(cour);
+
+        if (nouveauCour == null) throw new ImpossibleAjouterCommandeException("Impossible d'ajouter cette commande");
+
+        return new ResponseEntity<Cour>(cour, HttpStatus.CREATED);
     }
 }
 
